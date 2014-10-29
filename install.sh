6d802e08e50c45d562a92ee61b02d29dfd09a03c
guyfedwards <guy.edwards@veinteractive.com>
2014-10-29 00:00:04 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bash_profile .bashrc .gitconfig .gitignore .hushlogin .vimrc z.sh"        # list of files/folders to symlink in homedir
+files=".bash_profile .bashrc .gitconfig .gitignore .hushlogin .osx .vimrc z.sh"        # list of files/folders to symlink in homedir
 
 ##########
 
