46f14361729895b3c2bd35275b971743e9343168
guyfedwards <guyfedwards@gmail.com>
2016-05-10 13:59:56 +0100

    Use alias for vim so nvim is fine

--- tmux/session-scripts/tmux_fc.sh
+++ tmux/session-scripts/tmux_fc.sh
@@ -18,11 +18,11 @@ tmux resize-pane -t $SESSION_NAME:1.3 -U 10
 
 cd $APP_PATH
 tmux new-window -t $SESSION_NAME -a -n vim-fca
-tmux send-keys -t $SESSION_NAME:2.1 "vim" C-m
+tmux send-keys -t $SESSION_NAME:2.1 "v" C-m
 
 cd $FE_PATH
 tmux new-window -t $SESSION_NAME -a -n vim-fca-frontend
-tmux send-keys -t $SESSION_NAME:3.1 "vim" C-m
+tmux send-keys -t $SESSION_NAME:3.1 "v" C-m
 
 tmux select-window -t $SESSION_NAME:1
 tmux attach-session -t $SESSION_NAME
