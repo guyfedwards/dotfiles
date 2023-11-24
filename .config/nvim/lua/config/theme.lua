a0c1f42ff5ebbdcbf2db86911dbeae38d591a6bd
guyfedwards <guyfedwards@gmail.com>
2023-11-24 18:35:02 +0000

    darker bg for everforest

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -8,6 +8,11 @@ ef.setup({
   background = "hard",
   ui_contrast = "high",
   -- transparent_background_level = 2,
+  colours_override = function (palette)
+    -- palette.bg0 = "#21272B"
+    palette.bg0 = "#1D2226"
+    palette.bg1 = "#21272B"
+  end
 })
 
 local palette = colors.generate_palette(ef.config, vim.o.background)
