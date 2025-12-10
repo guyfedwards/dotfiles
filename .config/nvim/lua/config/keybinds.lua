164c47769a038a7c617feff000e508969949ca3d
guyfedwards <guyfedwards@gmail.com>
2025-12-10 13:56:52 +0000

    chore: get :Rg working

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -1,3 +1,4 @@
+-- Plugin specific keybinds live in the respective plugins/*.lua file
 local g = vim.g
 local k = vim.keymap
 local cmd = cmd
@@ -5,7 +6,7 @@ local cmd = cmd
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 g.mapleader = ","
-g.maplocalleader = "\\"
+g.maplocalleader = ","
 
 -- leader shortcuts
 k.set("", "<leader><leader>", "<c-^>")
@@ -48,29 +49,3 @@ k.set("n", "k", "gk", { noremap = true })
 
 -- highlight last inserted text
 k.set("n", "gV", "`[v`]", { noremap = true })
-
--- \ calls :Rg, if visual selection, call :Rg <selection>
-k.set("v", "\\", function()
-	local t = get_visual_selection()
-	cmd("Rg " .. t)
-end, { noremap = true })
-
-function get_visual_selection()
-	-- exit visual mode so '< and '> are populated correctly
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "x", true)
-
-	local s_start = vim.fn.getpos("'<")
-	local s_end = vim.fn.getpos("'>")
-
-	local n_lines = math.abs(s_end[2] - s_start[2]) + 1
-	local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
-
-	lines[1] = string.sub(lines[1], s_start[3], -1)
-
-	if n_lines == 1 then
-		lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
-	else
-		lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
-	end
-	return table.concat(lines, "\n")
-end
